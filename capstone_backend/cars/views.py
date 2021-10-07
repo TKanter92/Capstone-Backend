b2704039d9de97b6d78358ba5610028656596f33
TKanter92 <TylerKanter@yahoo.com>
2021-10-07 12:02:15 -0500

    created authentication to allow for users who are authorized to retrieve sample data of Cars with get request

--- capstone_backend/cars/views.py
+++ capstone_backend/cars/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Car
+from .serializers import CarSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class CarList(APIView):
+
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
