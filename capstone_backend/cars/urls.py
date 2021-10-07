1ec45c92aef409c3bfd4e81b734b2f299dae69ad
TKanter92 <TylerKanter@yahoo.com>
2021-10-07 14:46:03 -0500

    Created View Functions in Car App Views File + Created URLS File for the Cars App

--- capstone_backend/cars/urls.py
+++ capstone_backend/cars/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from cars import views
 
 urlpatterns = [
-    path('', views.CarList.as_view())
+    path('all/', views.get_all_cars),
+    path('', views.user_cars)
 ]
